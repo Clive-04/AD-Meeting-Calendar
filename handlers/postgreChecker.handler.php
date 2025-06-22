b0aed9289547880963b72470444486e92dfafce6
Clive-04 <202310384@fit.edu.ph>
2025-06-22 22:27:23 +0800

    fix: changed type on __DIR__

--- handlers/postgreChecker.handler.php
+++ handlers/postgreChecker.handler.php
@@ -1,5 +1,5 @@
 <?php
-require_once _DIR_ . '/../utils/envSetter.util.php';
+require_once __DIR__ . '/../utils/envSetter.util.php';
 $host = $pgConfig['host'];
 $port = $pgConfig['port'];
 $username = $pgConfig['user'];
