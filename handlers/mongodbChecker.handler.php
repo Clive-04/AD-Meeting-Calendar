8fd1c472a72c9dc1e3195dbb9a9c885dd9eedfed
Clive-04 <202310384@fit.edu.ph>
2025-06-22 21:09:48 +0800

    fix: change database port

--- handlers/mongodbChecker.handler.php
+++ handlers/mongodbChecker.handler.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:23567");
+    $mongo = new MongoDB\Driver\Manager("mongodb://host.docker.internal:27017");
 
     $command = new MongoDB\Driver\Command(["ping" => 1]);
     $mongo->executeCommand("admin", $command);
