daf538dabda4aff8c26293f5f0c95f5504f10027
Clive-04 <clivebenito21@gmail.com>
2025-07-08 12:31:12 +0800

    feat: add initial user static data for testing purposes

---
+++ staticDatas/dummies/users.staticData.php
@@ -0,0 +1,18 @@
+<?php
+// the table of users are composed of following columns: id, username, first_name, last_name, password, role
+return [
+    [
+        'username' => 'john.smith',
+        'first_name' => 'John',
+        'last_name' => 'Smith',
+        'password' => 'p@ssW0rd1234',
+        'role' => 'designer'
+    ],
+    [
+        'username' => 'admin.user',
+        'first_name' => 'Admin',
+        'last_name' => 'User',
+        'password' => 'admin123',
+        'role' => 'admin'
+    ],
+];
