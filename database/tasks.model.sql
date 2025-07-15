4c8b7dcaa5f1b34512e4eb1d0bae0201b07af328
Clive-04 <clivebenito21@gmail.com>
2025-07-15 12:23:29 +0800

    fix(database): fixed database code to function

--- database/tasks.model.sql
+++ database/tasks.model.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS tasks (
-    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-    project_id uuid NOT NULL REFERENCES projects (id) ON DELETE CASCADE,
-    assigned_to uuid REFERENCES users (id) ON DELETE SET NULL,
-    title varchar(255) NOT NULL,
-    description text,
-    status varchar(50) DEFAULT 'pending',
-    due_date date
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    project_id UUID NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+    assigned_to INTEGER REFERENCES users(id) ON DELETE SET NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    status VARCHAR(50) DEFAULT 'pending',
+    due_date DATE
 );
